feef4f44fe6480bc1efda27e6d7f002c769f4cfb
Yasith-Mayura <mayurayasith@gmail.com>
2024-07-16 17:46:12 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        constraints = [
+            (nums, '[0-9]'),
+            (lowercase, '[a-z]'),
+            (uppercase , '[A-Z]')
+        ]        
+    return password
+    
+# new_password = generate_password(8)
+# print(new_password)
+
+pattern = '[^a-z]t'
+quote = 'Not all those who wander are lost.'
+# print(re.findall(pattern, quote))
